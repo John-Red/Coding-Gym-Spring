a1f29951d0df3eaac59d6ec5d354b793e0a8523a
EvgeniiRedin <v-eredin@ebsco.com>
2020-06-18 10:38:25 +0300

    init

--- src/main/java/spring/SomeObject.java
+++ src/main/java/spring/SomeObject.java
@@ -1,20 +1,9 @@
 package spring;
 
-import org.springframework.stereotype.Component;
+public class SomeObject {
 
-@Component
-@MeasureTime
-public class SomeObject implements BaseObject {
-
-    @Override
     public void doSomething() {
-        for (int i = 0; i < 10; i++) {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("working");
-        }
+        System.out.println("working");
+
     }
 }
